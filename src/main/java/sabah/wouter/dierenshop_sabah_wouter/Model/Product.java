3dbfb6d0f9795fa7eb2ac3d7861a7b591c84dd45
mobapp06 <kaisssabah@hotmail.com>
2019-12-16 15:49:09 +0100

    config file aangemaakt

--- src/main/java/sabah/wouter/dierenshop_sabah_wouter/Model/Product.java
+++ src/main/java/sabah/wouter/dierenshop_sabah_wouter/Model/Product.java
@@ -1,4 +1,49 @@
 package sabah.wouter.dierenshop_sabah_wouter.Model;
 
-public class Product {
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+
+@Entity
+    public class Product {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+
+        @NotBlank
+        private String name;
+
+
+        @NotNull
+        @DecimalMin(value = "0.50",message = "moet groter zijn dan 0.5")
+        private BigDecimal price;
+
+        @NotBlank
+        @Size(min = 4, max = 400)
+        @Column(columnDefinition = "text")
+        private String description;
+
+
+        @NotBlank
+        private int rating;
+
+        private String imgUrl;
+
+        private String category;
+
+        @NotNull
+        private int stock;
+
+        private int delivery;
+
+
+
+
+
+
+
 }
