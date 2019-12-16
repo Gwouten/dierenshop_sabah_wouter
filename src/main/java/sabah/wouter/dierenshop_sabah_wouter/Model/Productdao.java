976d8593dc5fc994d7575896d3a65c8ff1012f2c
mobapp06 <kaisssabah@hotmail.com>
2019-12-16 16:12:06 +0100

    Dao aanmaken

---
+++ src/main/java/sabah/wouter/dierenshop_sabah_wouter/Model/Productdao.java
@@ -0,0 +1,7 @@
+package sabah.wouter.dierenshop_sabah_wouter.Model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Productdao extends CrudRepository <Product,Integer> {
+
+}
