160065ec79dbd0ff4f7ad1287a5fc8d8501da52c
mobapp06 <kaisssabah@hotmail.com>
2019-12-16 16:56:58 +0100

    index

---
+++ src/main/java/sabah/wouter/dierenshop_sabah_wouter/Controller/IndexController.java
@@ -0,0 +1,44 @@
+package sabah.wouter.dierenshop_sabah_wouter.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import sabah.wouter.dierenshop_sabah_wouter.Model.Product;
+import sabah.wouter.dierenshop_sabah_wouter.Model.Productdao;
+
+import javax.validation.Valid;
+
+@Controller
+class IndexController {
+
+    @Autowired
+    Productdao dao;
+
+    @ModelAttribute(value = "alleProducten")
+    public Iterable<Product> getAllProducts(){
+        return dao.findAll();
+    }
+
+
+    @RequestMapping(value = {"","/","/index"},method = RequestMethod.GET)
+    public String showIndex(ModelMap map) {
+        return "index";
+    }
+
+    @RequestMapping(value = {"","/","/index"},method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute(name="nieuweProduct") @Valid Product nieuweProduct, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return "new";
+        dao.save(nieuweProduct);
+        return "redirect:/index";
+
+
+
+
+    }
+}
+
