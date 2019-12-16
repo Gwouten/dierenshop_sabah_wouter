736074ac2b372fbfca2e54ce1fff6a0c5b79a435
mobapp06 <kaisssabah@hotmail.com>
2019-12-16 16:01:34 +0100

    versie 2 config file

--- src/main/java/sabah/wouter/dierenshop_sabah_wouter/Model/Product.java
+++ src/main/java/sabah/wouter/dierenshop_sabah_wouter/Model/Product.java
@@ -17,7 +17,6 @@ import java.math.BigDecimal;
         @NotBlank
         private String name;
 
-
         @NotNull
         @DecimalMin(value = "0.50",message = "moet groter zijn dan 0.5")
         private BigDecimal price;
@@ -27,23 +26,93 @@ import java.math.BigDecimal;
         @Column(columnDefinition = "text")
         private String description;
 
-
         @NotBlank
         private int rating;
 
+        @NotBlank
         private String imgUrl;
 
+        @NotBlank
         private String category;
 
         @NotNull
         private int stock;
 
+        @NotBlank
         private int delivery;
 
+    public Product() {
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public BigDecimal getPrice() {
+        return price;
+    }
 
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(int delivery) {
+        this.delivery = delivery;
+    }
 }
