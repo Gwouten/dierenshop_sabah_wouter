3e5e7f3eca0e9b00ef368c16dde626ca12618635
mobapp15 <mobapp15@student.ehb.be>
2019-12-16 15:05:14 +0100

    Initial commit

---
+++ src/main/java/sabah/wouter/dierenshop_sabah_wouter/DierenshopSabahWouterApplication.java
@@ -0,0 +1,13 @@
+package sabah.wouter.dierenshop_sabah_wouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DierenshopSabahWouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DierenshopSabahWouterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sabah/wouter/dierenshop_sabah_wouter/DierenshopSabahWouterApplicationTests.java
@@ -0,0 +1,13 @@
+package sabah.wouter.dierenshop_sabah_wouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DierenshopSabahWouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
