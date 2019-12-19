307bfedcebc5123833ab3a03c04716e7355c5c7a
mobapp06 <kaisssabah@hotmail.com>
2019-12-19 15:13:49 +0100

    controllers filters

--- src/main/java/sabah/wouter/dierenshop_sabah_wouter/Controller/CategoryController.java
+++ src/main/java/sabah/wouter/dierenshop_sabah_wouter/Controller/CategoryController.java
@@ -1,9 +1,34 @@
 package sabah.wouter.dierenshop_sabah_wouter.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import sabah.wouter.dierenshop_sabah_wouter.Model.Product;
+import sabah.wouter.dierenshop_sabah_wouter.Model.ProductDAO;
+
+
 
 @Controller
 public class CategoryController {
 
 
-}
+        @Autowired
+        ProductDAO dao;
+
+        @ModelAttribute(value = "alleDieren")
+        public Iterable<Product> findAll() {
+            return dao.findAll();
+        }
+
+        @RequestMapping(value = {"/categoryfilter/{category}"}, method = RequestMethod.GET)
+        public String showCategory(ModelMap map , @PathVariable(value = "category") String category) {
+            map.addAttribute("Mycategory", dao.findByCategory(category));
+            return "categoryfilter";
+        }
+
+
+    }
